33993d53969c2417fef79a3982e08242a733b99d
Chris <90427756+primeseven1@users.noreply.github.com>
2023-03-14 12:43:04 -0700

    Add files via upload
    Added sdkKeyboard.cpp and sdkKeyboard.h and .cpp files in the include directory

---
+++ Remove Unwanted Weapons/include/ScriptHookV/sdkKeyboard.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, BOOL isWithAlt, BOOL wasDownBefore, BOOL isUpNow);
+bool IsKeyDown(DWORD key);
+bool IsKeyJustUp(DWORD key, bool exclusive);

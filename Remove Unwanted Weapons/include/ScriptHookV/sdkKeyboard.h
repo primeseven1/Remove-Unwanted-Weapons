c7ebf6fa0a9491d2c61e127468d6a4386f795a2d
Chris <90427756+primeseven1@users.noreply.github.com>
2023-03-14 19:16:48 -0700

    Delete Remove Unwanted Weapons directory

--- Remove Unwanted Weapons/include/ScriptHookV/sdkKeyboard.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, BOOL isWithAlt, BOOL wasDownBefore, BOOL isUpNow);
-bool IsKeyDown(DWORD key);
-bool IsKeyJustUp(DWORD key, bool exclusive);

04421fac7c269ff42a859a0b4eb9f8f8d70176f3
Chris <90427756+primeseven1@users.noreply.github.com>
2023-04-10 05:06:08 -0700

    Update scriptMain.cpp

--- RemoveUnwantedWeapons/src/Main/scriptMain.cpp
+++ RemoveUnwantedWeapons/src/Main/scriptMain.cpp
@@ -5,6 +5,7 @@
 static void waitForStory()
 {
 	// The story has loaded once the player entity exists
+	// You may be on the loading screen, but that doesn't really matter
 	while (!ENTITY::DOES_ENTITY_EXIST(PLAYER::PLAYER_PED_ID()))
 	{
 		scriptWait(0);

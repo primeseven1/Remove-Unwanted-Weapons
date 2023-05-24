3724a085b922156c4ea5202c4dfa92aa255f56cd
Chris <90427756+primeseven1@users.noreply.github.com>
2023-05-24 01:43:56 -0700

    Update logging.cpp

--- Remove-Unwanted-Weapons/src/files/logging.cpp
+++ Remove-Unwanted-Weapons/src/files/logging.cpp
@@ -12,11 +12,12 @@ void genLogFile(const char* loggingInfo)
 	if (!loggingFile.is_open())
 		return;
 
+	// Get the time so that it can be logged to the file
 	auto currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 	struct tm timeInfo;
 	localtime_s(&timeInfo, &currentTime);
 	std::stringstream ss;
-	ss << std::put_time(&timeInfo, "%Y-%m-%d %H:%M:%S");
+	ss << std::put_time(&timeInfo, "%Y-%m-%d %H:%M:%S"); // Formatting as a string
 
 	loggingFile << loggingInfo << "| Time:" << ss.str() << "\n";
 	loggingFile.close();
